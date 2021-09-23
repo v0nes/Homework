868108901590a59ccb1ce7747c8cf631ac67b202
Ivan Dukhin <vipzombi101@gmail.com>
2021-09-24 00:21:12 +0300

    add another homework
    wrong option choosed

--- homework/homework/main.c
+++ homework/homework/main.c
@@ -1,93 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#define ARRAY_LEN 64
+#include <limits.h>
+#include <stdbool.h>
 int main()
 {
-    char buffer, arr[ARRAY_LEN];
-    int k = 0;
-    // flag to check for the presence of the first number
-    uint8_t first_number_exist = 0;
-    // flag to check for the presence of a vertical bar
-    uint8_t second_number_check = 0;
-    // flag to check for the presence of the second number
-    uint8_t second_number_exist = 0;
-    while (
-        (buffer = getchar()) != EOF &&
-        buffer != '\n' &&
-        buffer != '\0')
-    {
-        switch (buffer)
-        {
-            case '-':
-                if (k == 0)
-                {
-                    arr[k] = buffer;
-                }
-                else
-                {
-                    printf("\nInput error");
-                    exit(-1);
-                }
-                break;
+	int8_t zero_flag = 0; // delites all zeros at the beggining of the number
+	int decimal;
+	int int_size = sizeof(int) * CHAR_BIT;
+	int num; // number of signs
+	bool binary[sizeof(int) * CHAR_BIT];
+	scanf_s("%d%n", &decimal, &num);
+	if (num > 11 && int_size == 32)
+	{
+		printf("A lot of signs");
+		exit(-1);
+	}
+	if (num > 20 && int_size == 64)
+	{
+		printf("A lot of signs");
+		exit(-1);
+	}
+	if (decimal< 0)
+	{
+		decimal *= -1;
+		printf("-");
+	}
 
-            case ' ':
-                break;
-
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                arr[k] = buffer;
-                if (second_number_check == 0)
-                {
-                    first_number_exist = 1;
-                }
-                if (second_number_check == 1)
-                {
-                    second_number_exist = 1;
-                }
-                k++;
-                break;
-
-            case '|':
-                if(second_number_check == 1)
-                {
-                    printf("\nInput error");
-                    exit(-1);
-                }
-                arr[k] ='.';
-                k++;
-                second_number_check = 1;
-                break;
-
-            default:
-                printf("\nInput error");
-                exit(-1);
-        }
-
-        if (k >= ARRAY_LEN)
-        {
-            printf("\nInput error");
-            exit(-1);
-        }
-    }
-    arr[k] = '\0';
-    if (first_number_exist == 1 &&
-        second_number_exist == 1)
-    {
-        printf("%s", arr);
-    }
-    else
-    {
-        printf("\nmissing second number");
-        exit(-1);
-    }
-    exit(0);
+	for(int j = int_size - 1 ; j >= 0; j--)
+	{
+		binary[j] = ((decimal & (1u << j)) >> j);
+		if(binary[j] == 1)
+		{
+			zero_flag = 1;
+		}
+		if (zero_flag == 1)
+		{
+			printf("%d", binary[j]);
+		}
+	}
+	exit(0);
 }
